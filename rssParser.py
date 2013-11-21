bc5ef2357c0bd53ec0913c62d0dcb637baaf89db
Flaxbeard <vitanovafc@gmail.com>
2013-11-20 18:55:10 -0600

    Adding comments

--- rssParser.py
+++ rssParser.py
@@ -2,6 +2,8 @@ import feedparser
 import socket
 import urllib2
 import os
+#tuple of tuples (tupleception!)
+#(Formatting code, name (only shown in python script), folder name, link)
 feeds = [
     ("NYT","the New York Times - Africa","NYT-Africa","http://www.nytimes.com/services/xml/rss/nyt/Africa.xml"),
     ("NYT","the New York Times - Asia and the Pacific","NYT-Asia/Pacific","http://www.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"),
@@ -13,15 +15,17 @@ feeds = [
     ]
 timeout = 120
 socket.setdefaulttimeout(timeout)
+#For every feed...
 for (feed_type, feed_name, feed_abv, feed_url) in feeds:
     d = feedparser.parse(feed_url)
     newArticles = 0
     print "Checking " + feed_name
+    #For every feed item
     for s in d.entries:
 
-        
         import urllib2
         thingy = 0
+        #Remove stupid url additions (usually rss referral analytics things)
         if (feed_type == "NYT"):
             thingy = s.link.index('?partner')
         if (feed_type == "BBC"):
@@ -32,19 +36,27 @@ for (feed_type, feed_name, feed_abv, feed_url) in feeds:
             if not(bad == -1):
                 continue
             thingy = s.link.find('#sa')
+        #truncates title to first 50 letters
         filetitle = s.title[0:50]
+        #remove bad characters
         keepcharacters = (' ','.','_','-')
         filetitle =  "".join(c for c in filetitle if c.isalnum() or c in keepcharacters).rstrip()
+        #checks if article has been downloaded
         if not os.path.exists('extemp/' + feed_abv +'/' + filetitle + '.html'):
+            #New York Times formatting code
             if (feed_type == "NYT"):
+                #all pages on one page
                 url = s.link[0:thingy] + "?pagewanted=all"
+                #open url
                 usock = urllib2.build_opener(urllib2.HTTPCookieProcessor).open(url)
+                #create directory
                 if not os.path.exists('extemp/' + feed_abv):
                     os.makedirs('extemp/' + feed_abv)
                 output = open('extemp/' + feed_abv +'/' + filetitle + '.html','wb')
                 test = usock.read()
                 doarticle = 'false'
                 hstart = test.find('<body')
+                #remove ads and stuff
                 if hstart == -1:
                     hstart = 0
                     doarticle = 'true'
@@ -54,15 +66,23 @@ for (feed_type, feed_name, feed_abv, feed_url) in feeds:
                 epos = test.find('<!--close abColumn')
                 if epos == -1:
                     epos = test.find('<!-- end aCol')
+                #save page
                 if doarticle == 'false':
+                    #known NYT format (normal article, The Lede)
                     output.write(test[0:hstart] + "<body>" + test[spos:epos] + "</body></html>")
                 else:
+                    #unknown format
                     output.write(test)
+            #BBC formatting code
             elif (feed_type == "BBC"):
+                #print mode
                 url = s.link[0:thingy] + "?print=true"
+                #open url
                 usock = urllib2.build_opener(urllib2.HTTPCookieProcessor).open(url)
+                #create directory
                 if not os.path.exists('extemp/' + feed_abv):
                     os.makedirs('extemp/' + feed_abv)
+                #save page
                 output = open('extemp/' + feed_abv +'/' + filetitle + '.html','wb')
                 test = usock.read()
                 output.write(test)
